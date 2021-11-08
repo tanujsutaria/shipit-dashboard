45d977e912cb7c4ff26af89130e47384750d2947
tsutaria <tanuj.sutaria@agilent.com>
2021-11-08 11:52:36 -0800

    Added basic routes for views

--- api/views.py
+++ api/views.py
@@ -2,4 +2,39 @@ from flask import Blueprint,jsonify,request
 from . import db
 from .models import Order
 
+main = Blueprint('main', __name__)
+
+@main.route('/orders', methods = ['GET'])
+def get_orders():
+    """"""
+    orders = []
+    orders_list = Order.query.all()
+    for order in orders_list:
+        orders.append({
+            'pk' : order.pk,
+            'order_number' : order.order_number,
+            'order_status' : order.order_status,
+            'order_created_date' : order.order_created_date,
+            'order_finished_date' : order.order_finished_date,
+            'order_target_finish_date' : order. order_target_finish_date,
+            'order_late_comment' : order.order_late_comment
+        })
+    return jsonify({'Orders':orders})
+
+@main.route('/add_order', methods = ['POST'])
+def add_order():
+    """POST request connected to webhook that automatically adds the order into the database"""
+    order_data = request.get_json()
+    new_order = Order(
+        pk = order_data['pk'],
+        order_number = order_data['order_number'],
+        order_status = order_data['order_status'],
+        order_created_date = order_data['order_created_date'],
+        order_finished_date = order_data['order_finished_date'],
+        order_target_finish_date = order_data['order_target_finish_date'],
+        order_late_comment = order_data['order_late_comment']
+    )
+    db.session.add(new_order)
+    db.session.commit()
+    return 'Done', 201
 
