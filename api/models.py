95c2bee7f7260db00292a7b66e008187374654c4
tsutaria <tanuj.sutaria@agilent.com>
2021-11-08 11:17:00 -0800

    Setting up flask backend

---
+++ api/models.py
@@ -0,0 +1,13 @@
+from . import db
+
+class Order(db.Model):
+    """Creates the elements of the Order table in the sqlite3 db"""
+    id = db.Column(db.Integer, primary_key = False)
+    pk = db.Column(db.Long, primary_key = True)
+    order_number = db.Column(db.Integer)
+    order_status = db.Column(db.String)
+    order_created_date = db.Column(db.String)
+    order_finished_date = db.Column(db.String)
+    order_target_finish_date = db.Column(db.String)
+    
+
---
+++ api/views.py
@@ -0,0 +1,5 @@
+from flask import Blueprint,jsonify,request
+from . import db
+from .models import Order
+
+
