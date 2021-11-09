649a144823f01ab026c6934a557adb760910bdc8
Tanuj Sutaria <tanujsutaria@Tanujs-MacBook-Pro-2.local>
2021-11-08 21:36:48 -0800

    Fixed update orders route and now it works properly- tested with POSTMAN'

--- api/views.py
+++ api/views.py
@@ -39,18 +39,20 @@ def add_order():
     return 'Done', 201
 
 @main.route('/update_order', methods = ['POST'])
-def add_order():
-    """POST request connected to webhook that automatically adds the order into the database"""
+def update_order():
+    """POST request connected to webhook that automatically update the order in the database"""
     order_data = request.get_json()
-    new_order = Order(
-        pk = order_data['pk'],
-        order_number = order_data['order_number'],
-        order_status = order_data['order_status'],
-        order_created_date = order_data['order_created_date'],
-        order_finished_date = order_data['order_finished_date'],
-        order_target_finish_date = order_data['order_target_finish_date'],
-        order_late_comment = order_data['order_late_comment']
-    )
-    db.session.bulk_update_mappings(new_order)
-    db.session.commit()
-    return 'Done', 201
+    if(order_data['pk']):
+        order = Order.query.filter_by(pk=order_data['pk'])
+        order.update(dict(
+                order_number = order_data['order_number'],
+                order_status = order_data['order_status'],
+                order_created_date = order_data['order_created_date'],
+                order_finished_date = order_data['order_finished_date'],
+                order_target_finish_date = order_data['order_target_finish_date'],
+                order_late_comment = order_data['order_late_comment']
+                ))
+        db.session.commit()
+        return 'Done', 201
+    else:
+        return "Please POST request with pk"
