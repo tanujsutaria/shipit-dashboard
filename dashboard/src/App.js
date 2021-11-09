6b384a43b63f1c5387eaaa1e321808d606b44999
Tanuj Sutaria <tanujsutaria@Tanujs-MacBook-Pro-2.local>
2021-11-08 22:58:35 -0800

    Updated react up to check if browswer was displaying correctly- it is; will create table tomorrow

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import { Orders } from './components/Orders';
+
 
 function App() {
+  const [orders, setOrders] = useState([]);
   useEffect(() => {
     fetch("/orders").then(response =>
       response.json().then(data => {
-        console.log(data)
+        setOrders(data.Orders);
       }));
   },[]);
   return (
     <div className="App">
+      <Orders orders = {orders}/>
     </div>
   );
 }
